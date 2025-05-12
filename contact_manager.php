ea88951fdb7f1c40e4eed47b394c56017ff8bff8
asifbuet047 <asifbuet047@gmail.com>
2025-05-12 22:39:27 +0600

    First commit

---
+++ contact_manager.php
@@ -0,0 +1,6 @@
+<?php
+// Basic PHP script
+
+// Display a welcome message
+echo "Welcome to the Contact Manager!";
+?>
---
+++ quiz_app.php
@@ -0,0 +1,7 @@
+<?php
+// Basic PHP script
+
+// Display a welcome message
+echo "Welcome to the Quiz App!";
+
+?>
